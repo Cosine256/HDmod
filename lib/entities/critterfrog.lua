00a479b1017be00269b0142f0592c8d60cfc44d8
doofus9341 <15894621+doofus9341@users.noreply.github.com>
2023-07-27 22:06:07 -0400

    Fix frog audio resources
    Reprocessed problematic audio files so that they work in-game.
    Changes critterfrog to use wav files again.

--- lib/entities/critterfrog.lua
+++ lib/entities/critterfrog.lua
@@ -36,13 +36,13 @@ do
 end
 
 local jump_sound = {
-    create_sound('res/sounds/critterfrog1.ogg'),
-    create_sound('res/sounds/critterfrog2.ogg'),
-    create_sound('res/sounds/critterfrog3.ogg'),
-    create_sound('res/sounds/critterfrog4.ogg'),
-    create_sound('res/sounds/critterfrog5.ogg'),
-    create_sound('res/sounds/critterfrog6.ogg'),
-    create_sound('res/sounds/critterfrog7.ogg')
+    create_sound('res/sounds/critterfrog1.wav'),
+    create_sound('res/sounds/critterfrog2.wav'),
+    create_sound('res/sounds/critterfrog3.wav'),
+    create_sound('res/sounds/critterfrog4.wav'),
+    create_sound('res/sounds/critterfrog5.wav'),
+    create_sound('res/sounds/critterfrog6.wav'),
+    create_sound('res/sounds/critterfrog7.wav')
 }
 
 local CRITTERFROG_STATE <const> = {
