936277fa5ee32e9ff69d9d01f69884e395cfe770
Super Ninja Fat <paulmlindberg@gmail.com>
2023-08-12 12:06:30 -0400

    Add scrolling bg layers to credits

--- lib/surface.lua
+++ lib/surface.lua
@@ -31,26 +31,45 @@ do
     surface_foreground_hard_texture_id = define_texture(surface_foreground_hard_texture_def)
 end
 
-local function create_surface_layer_looping(x)
-    local bg_surface_layer_1 = get_entity(spawn_entity(ENT_TYPE.BG_SURFACE_LAYER, 0, 0, LAYER.FRONT, 0, 0))
-    bg_surface_layer_1.relative_x, bg_surface_layer_1.relative_y = x, 111.150--seems to be 10 higher than intro
-    bg_surface_layer_1.width = 60
-    bg_surface_layer_1.height = 3.75
-    bg_surface_layer_1.hitboxx = 30
-    bg_surface_layer_1.hitboxy = 1.875
-    bg_surface_layer_1.tile_width = 4
-    bg_surface_layer_1.tile_height = 1
-    bg_surface_layer_1.animation_frame = 3
-    bg_surface_layer_1:set_post_update_state_machine(
+local function _create_surface_layer_looping_sub(x, y, depth)
+    local bg_surface_layer = get_entity(spawn_entity(ENT_TYPE.BG_SURFACE_LAYER, 0, 0, LAYER.FRONT, 0, 0))
+    bg_surface_layer.relative_x, bg_surface_layer.relative_y = x, y--seems to be 10 higher than intro
+    bg_surface_layer.width = 60
+    bg_surface_layer.height = 3.75
+    bg_surface_layer.hitboxx = 30
+    bg_surface_layer.hitboxy = 1.875
+    bg_surface_layer.tile_width = 4
+    bg_surface_layer.tile_height = 1
+    bg_surface_layer.animation_frame = 3
+    bg_surface_layer:set_draw_depth(depth)
+    if state.win_state == WIN_STATE.HUNDUN_WIN then
+        bg_surface_layer:set_texture(surface_hard_texture_id)
+    end
+
+    local speed = 0.02
+    if depth == decorlib.SURFACE_BG_DEPTH.MID_BACKGROUND then
+        speed = 0.015
+        bg_surface_layer.animation_frame = 2
+    elseif depth == decorlib.SURFACE_BG_DEPTH.BACK_BACKGROUND then
+        speed = 0.01
+        bg_surface_layer.animation_frame = 1
+    end
+    
+    bg_surface_layer:set_post_update_state_machine(
         ---@param self Movable | Entity | BGSurfaceLayer
         function (self)
             if self.relative_x >= 93 then
                 self.relative_x = self.relative_x - 119.97
             else
-                self.relative_x = self.relative_x + 0.02
+                self.relative_x = self.relative_x + speed
             end
         end
     )
+end
+
+local function create_surface_layer_looping(x, y, depth)
+    _create_surface_layer_looping_sub(x, y, depth)
+    _create_surface_layer_looping_sub(x - 60, y, depth)
 end
 
 function module.build_credits_surface()
@@ -77,10 +96,9 @@ function module.build_credits_surface()
         end
     )
 
-    create_surface_layer_looping(25)
-    create_surface_layer_looping(25 - 60)
-    local bg_surface_layer_2
-    local bg_surface_layer_3
+    create_surface_layer_looping(25, 111.15, decorlib.SURFACE_BG_DEPTH.BACKGROUND)
+    create_surface_layer_looping(25, 112.6, decorlib.SURFACE_BG_DEPTH.MID_BACKGROUND)
+    create_surface_layer_looping(25, 113.6, decorlib.SURFACE_BG_DEPTH.BACK_BACKGROUND)
     if state.win_state == WIN_STATE.HUNDUN_WIN then
         bg_sky:set_texture(sky_hard_texture_id)
     end
