fe21735c2741b8e377bf7f9fcdbfd55e1ff39ddf
Super Ninja Fat <paulmlindberg@gmail.com>
2023-08-19 20:28:25 -0400

    Add smoke particle effects
    Adjust volcano scale and player ejection point

--- lib/scores.lua
+++ lib/scores.lua
@@ -67,7 +67,7 @@ set_callback(function (ctx, draw_depth)
             ctx:draw_world_texture(hard and sky_hard_texture_id or TEXTURE.DATA_TEXTURES_BASE_SKYNIGHT_0, 0, 0, Quad:new(AABB:new(9.24, 110.09, 30.60, 98.08)), Color:white(), WORLD_SHADER.TEXTURE_COLOR)
         end
         if draw_depth == decorlib.CREDITS_VOLCANO_DEPTH.VOLCANO then
-            ctx:draw_world_texture(hard and volcano_hard_texture_id or volcano_texture_id, 0, 0, Quad:new(AABB:new(12.24, 107.09, 30.60, 98.08)), Color:white(), WORLD_SHADER.TEXTURE_COLOR)
+            ctx:draw_world_texture(hard and volcano_hard_texture_id or volcano_texture_id, 0, 0, Quad:new(AABB:new(10.24, 107.09, 31.60, 98.08)), Color:white(), WORLD_SHADER.TEXTURE_COLOR)
         end
     end
 end, ON.RENDER_PRE_DRAW_DEPTH)
@@ -75,7 +75,32 @@ end, ON.RENDER_PRE_DRAW_DEPTH)
 --smoke particles
 -- rock, gradually move up, growing in size, rotating
 -- TEXTURE.DATA_TEXTURES_SHADOWS_0
+-- animation_frame = 3
 -- width, height = 3.5, 3.5
+local function create_smoke_particle(x, y, animation_frame, rotation)
+    ---@type Movable | Entity
+    local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, x, y, LAYER.FRONT, 0.0265, 0.0125))
+    entity.flags = set_flag(entity.flags, ENT_FLAG.NO_GRAVITY)
+    entity.flags = set_flag(entity.flags, ENT_FLAG.PASSES_THROUGH_EVERYTHING)
+    entity:set_texture(TEXTURE.DATA_TEXTURES_SHADOWS_0)
+    entity.animation_frame = animation_frame
+    entity.width, entity.height = 3, 3
+    entity:set_draw_depth(decorlib.CREDITS_VOLCANO_DEPTH.PARTICLES)
+    entity.angle = rotation
+    local alpha_timeout = 10
+    entity:set_post_update_state_machine(function (self)
+        self.width, self.height = self.width*1.005, self.height*1.005
+        self.angle = self.angle + 0.015
+        if alpha_timeout < 90 then
+            self.color = Color:new(1, 1, 1, alpha_timeout/100)
+            alpha_timeout = alpha_timeout + 1
+        end
+        if VOLCANO_DISAPPEAR
+        or self.y > 120 then
+            self:destroy()
+        end
+    end)
+end
 
 local function create_lava_particle(x, y, animation_frame, size, vx, vy, spin_rate, texture_id, animation)
     local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, x, y, LAYER.FRONT, vx, vy))
@@ -106,15 +131,16 @@ local function create_lava_particle(x, y, animation_frame, size, vx, vy, spin_ra
 end
 
 local function create_volcano_effects()
-    local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, 26.02, 106.46, LAYER.FRONT, 0, 0))
+    local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, 25.02, 106.06, LAYER.FRONT, 0, 0))
     entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
     entity.flags = set_flag(entity.flags, ENT_FLAG.NO_GRAVITY)
     commonlib.shake_camera(180, 480, 0.5, 1.5, 1.5, false)
-    
+
     ---@type CustomSound_play
     local rumble_sound = commonlib.play_vanilla_sound(VANILLA_SOUND.CUTSCENE_RUMBLE_LOOP, entity.uid, 0.25, true)
     local timeout = 160
-    local particles_timeout = 0
+    local lava_timeout = 0
+    local smoke_timeout = 0
     entity:set_post_update_state_machine(function (self)
         if timeout == 0 then -- erupt
             commonlib.play_vanilla_sound(VANILLA_SOUND.SHARED_EXPLOSION, self.uid, 0.003, false)
@@ -136,8 +162,7 @@ local function create_volcano_effects()
         end
 
         if not VOLCANO_DISAPPEAR then
-            --randomly spawn enemies, but pad them out so it isn't too spammy or empty
-            if particles_timeout <= 0 then
+            if lava_timeout <= 0 then
                 local y = entity.y + prng:random_float(PRNG_CLASS.PARTICLES)*1.5
                 local x = entity.x + prng:random_float(PRNG_CLASS.PARTICLES)*2.5
                 create_lava_particle(x, y, 51,
@@ -148,9 +173,15 @@ local function create_volcano_effects()
                     TEXTURE.DATA_TEXTURES_FX_SMALL3_0,
                     PARTICLE_ANIMATIONS.FLAME1
                 )
-                particles_timeout = prng:random_int(3, 7, PRNG_CLASS.PARTICLES)
+                lava_timeout = prng:random_int(3, 7, PRNG_CLASS.PARTICLES)
             else
-                particles_timeout = particles_timeout - 1
+                lava_timeout = lava_timeout - 1
+            end
+            if smoke_timeout <= 0 then
+                create_smoke_particle(entity.x, entity.y+1, 3, 0.05)
+                smoke_timeout = prng:random_int(30, 40, PRNG_CLASS.PARTICLES)
+            else
+                smoke_timeout = smoke_timeout - 1
             end
         end
 
@@ -159,12 +190,12 @@ local function create_volcano_effects()
 end
 
 local function create_flung_entity(texture_id, animation_frame, timeout, size, animation)
-    local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, 25.42, 106.15, LAYER.FRONT, 0, 0))
+    local entity = get_entity(spawn_entity(ENT_TYPE.ITEM_ROCK, 25.62, 106.35, LAYER.FRONT, 0, 0))
     entity:set_draw_depth(decorlib.CREDITS_VOLCANO_DEPTH.FLUNG_ENTS)
     entity:set_texture(texture_id)
     entity.animation_frame = animation_frame
     entity.flags = set_flag(entity.flags, ENT_FLAG.PASSES_THROUGH_EVERYTHING)
-    -- entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
+    entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
     entity.flags = set_flag(entity.flags, ENT_FLAG.NO_GRAVITY)
     entity.flags = set_flag(entity.flags, ENT_FLAG.FACING_LEFT)
     local gravity = 0.15
@@ -181,11 +212,24 @@ local function create_flung_entity(texture_id, animation_frame, timeout, size, a
         if timeout > 0 then
             timeout = timeout - 1
         elseif not flung then
-            -- self.flags = clr_flag(self.flags, ENT_FLAG.INVISIBLE)
+            self.flags = clr_flag(self.flags, ENT_FLAG.INVISIBLE)
             self.flags = clr_flag(self.flags, ENT_FLAG.NO_GRAVITY)
             self.velocityx = -0.033
-            self.velocityy = 0.04
+            self.velocityy = 0.055
             flung = true
+            
+            for i = 1, 10 do
+                local y = entity.y-- + prng:random_float(PRNG_CLASS.PARTICLES)*1.5
+                local x = entity.x-- + prng:random_float(PRNG_CLASS.PARTICLES)*2.5
+                create_lava_particle(x, y, 51,
+                    prng:random_float(PRNG_CLASS.PARTICLES)*0.75,
+                    prng:random_float(PRNG_CLASS.PARTICLES)*0.2-0.1,
+                    prng:random_float(PRNG_CLASS.PARTICLES)*0.1+0.1,
+                    0.01,
+                    TEXTURE.DATA_TEXTURES_FX_SMALL3_0,
+                    PARTICLE_ANIMATIONS.FLAME1
+                )
+            end
         else
             self.width, self.height = self.width*1.006, self.height*1.006
             self.angle = self.angle + 0.2
