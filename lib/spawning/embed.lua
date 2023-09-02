ca17d2f9604fdcd91832ff73b44ffa697a0a9487
Super Ninja Fat <paulmlindberg@gmail.com>
2023-09-02 14:27:47 -0400

    Use jay's game-accurate embed method

--- lib/spawning/embed.lua
+++ lib/spawning/embed.lua
@@ -1,15 +1,19 @@
 local module = {}
 
-function module.embed_item(enum, uid, frame)
-	local x, y, l = get_position(uid)
-	-- local ents = get_entities_at(0, 0, x, y, l, 0.1)
-	-- if (#ents > 1) then return end
+function module.embed_item(type, floor_uid, frame)
+	local x, y, l = get_position(floor_uid)
 	removelib.remove_embedded_at(x, y, l)
 
-	local entity = get_entity(spawn_entity_over(ENT_TYPE.ITEM_ALIVE_EMBEDDED_ON_ICE, uid, 0, 0))
-	entity.inside = enum
-	entity.animation_frame = frame
-	entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
+	local embed_ent = get_entity(spawn_entity_over(type, floor_uid, 0, 0))
+
+	-- attach_entity(floor_uid, embed_uid)
+	embed_ent.special_offsetx = 0
+	embed_ent.flags = set_flag(embed_ent.flags, ENT_FLAG.NO_GRAVITY)
+	embed_ent.flags = clr_flag(embed_ent.flags, ENT_FLAG.COLLIDES_WALLS)
+	embed_ent:set_draw_depth(9)
+	-- if not gold then
+		embed_ent.flags = set_flag(embed_ent.flags, ENT_FLAG.INVISIBLE)
+	-- end
 	return 0;
 end
 
