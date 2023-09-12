5f8f79697df89d9b8ac4ea1af5f696476bd0def9
doofus9341 <15894621+doofus9341@users.noreply.github.com>
2023-09-12 18:34:25 -0400

    Update Yama boss music
    Update Yama boss music to include new transition stems and revised phase 2.
    Restore BGM_Yama_Intro.

--- lib/music/hell/yamamusic.lua
+++ lib/music/hell/yamamusic.lua
@@ -2,8 +2,6 @@ local module = {}
 
 module.music_debug_print = false
 
-local stem_after_tran
-
 local function playerInRange(player)
     if player ~= nil then
         local px, py, layer = get_position(player)
@@ -14,7 +12,7 @@ local function playerInRange(player)
                 if mons.user_data.ent_type == HD_ENT_TYPE.MONS_YAMA_HEAD then
                     --[[
                         Yama's hands decide if a player is in range by checking if the players y position is greater than
-                        their own - 6. We're checking from Yama's heads y position so subtract an additional 1.15.
+                        their own - 6. We're checking from the y position of Yama's head so subtract an additional 1.15.
                     ]]
                     if py > mons.y - 7.15 then
                         return true
@@ -54,7 +52,7 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Yama phase2 = true.")
                     end
 
-                    return "1_p3"
+                    return "1p3"
                 end
 
                 if playerInRange(players[1].uid) then
@@ -62,15 +60,15 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Player moved into Yama's attack range.")
                     end
 
-                    return "1_p2"
+                    return "1p2"
                 end
 
-                return "1_p1"
+                return "1p1"
             end
         },
         {
-            id = "1_p1",
-            sound = create_sound("res/music/BGM_Yama_1_P1.ogg"),
+            id = "1p1",
+            sound = create_sound("res/music/BGM_Yama_1P1.ogg"),
             length = 8210,
             next_sound_id = function(ctx)
                 if isYamaPhaseTwo() then
@@ -78,7 +76,7 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Yama phase2 = true.")
                     end
 
-                    return "2_p3"
+                    return "2p3"
                 end
 
                 if playerInRange(players[1].uid) then
@@ -86,23 +84,15 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Player moved into Yama's attack range.")
                     end
 
-                    return "2_p2"
+                    return "2p2"
                 end
 
-                return "2_p1"
+                return "2p1"
             end
         },
         {
-            id = "p1_tran",
-            sound = create_sound("res/music/BGM_Yama_P1_Tran.ogg"),
-            length = 947,
-            next_sound_id = function(ctx)
-                return stem_after_tran
-            end
-        },
-        {
-            id = "1_p2",
-            sound = create_sound("res/music/BGM_Yama_1_P2.ogg"),
+            id = "1p2",
+            sound = create_sound("res/music/BGM_Yama_1P2.ogg"),
             length = 8210,
             next_sound_id = function(ctx)
                 if isYamaPhaseTwo() then
@@ -110,21 +100,67 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Yama phase2 = true.")
                     end
 
-                    return "2_p3"
+                    return "2p3"
                 end
 
-                return "2_p2"
+                return "2p2"
             end
         },
         {
-            id = "1_p3",
-            sound = create_sound("res/music/BGM_Yama_1_P3.ogg"),
+            id = "1p3",
+            sound = create_sound("res/music/BGM_Yama_1P3.ogg"),
             length = 8210,
-            next_sound_id = "2_p3"
+            next_sound_id = "2p3"
         },
         {
-            id = "2_p1",
-            sound = create_sound("res/music/BGM_Yama_2_P1.ogg"),
+            id = "2p1",
+            sound = create_sound("res/music/BGM_Yama_2P1.ogg"),
+            length = 5052,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "2px_3p3"
+                end
+
+                if playerInRange(players[1].uid) then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Player moved into Yama's attack range.")
+                    end
+
+                    return "2p2_3p2"
+                end
+
+                return "2p1_3p1"
+            end
+        },
+        {
+            id = "2p2",
+            sound = create_sound("res/music/BGM_Yama_2P2.ogg"),
+            length = 5052,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "2px_3p3"
+                end
+
+                return "2p2_3p2"
+            end
+        },
+        {
+            id = "2p3",
+            sound = create_sound("res/music/BGM_Yama_2P3.ogg"),
+            length = 5052,
+            next_sound_id = "2px_3p3"
+        },
+        {
+            id = "3p1",
+            sound = create_sound("res/music/BGM_Yama_3P1.ogg"),
             length = 5684,
             next_sound_id = function(ctx)
                 if isYamaPhaseTwo() then
@@ -132,8 +168,7 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Yama phase2 = true.")
                     end
 
-                    stem_after_tran = "3_p3"
-                    return "p1_tran"
+                    return "3p1_3p3"
                 end
 
                 if playerInRange(players[1].uid) then
@@ -141,44 +176,15 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Player moved into Yama's attack range.")
                     end
 
-                    stem_after_tran = "3_p2"
-                    return "p1_tran"
+                    return "3p1_3p2"
                 end
 
-                return "2_p1_tran_def"
+                return "3p1_4p1"
             end
         },
         {
-            id = "2_p1_tran_def",
-            sound = create_sound("res/music/BGM_Yama_2_P1_Tran_def.ogg"),
-            length = 947,
-            next_sound_id = "3_p1"
-        },
-        {
-            id = "2_p2",
-            sound = create_sound("res/music/BGM_Yama_2_P2.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "3_p3"
-                end
-
-                return "3_p2"
-            end
-        },
-        {
-            id = "2_p3",
-            sound = create_sound("res/music/BGM_Yama_2_P3.ogg"),
-            length = 6631,
-            next_sound_id = "3_p3"
-        },
-        {
-            id = "3_p1",
-            sound = create_sound("res/music/BGM_Yama_3_P1.ogg"),
+            id = "3p2",
+            sound = create_sound("res/music/BGM_Yama_3P2.ogg"),
             length = 5684,
             next_sound_id = function(ctx)
                 if isYamaPhaseTwo() then
@@ -186,8 +192,29 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Yama phase2 = true.")
                     end
 
-                    stem_after_tran = "4_p3"
-                    return "p1_tran"
+                    return "3p2_4p3"
+                end
+
+                return "3p2_4p2"
+            end
+        },
+        {
+            id = "3p3",
+            sound = create_sound("res/music/BGM_Yama_3P3.ogg"),
+            length = 5684,
+            next_sound_id = "3p3_4p3"
+        },
+        {
+            id = "4p1",
+            sound = create_sound("res/music/BGM_Yama_4P1.ogg"),
+            length = 6631,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "5p3"
                 end
 
                 if playerInRange(players[1].uid) then
@@ -195,178 +222,179 @@ module.YAMA_BOSS_CUSTOM_MUSIC = {
                         print("[Yama Music Debug] Player moved into Yama's attack range.")
                     end
 
-                    stem_after_tran = "4_p2"
-                    return "p1_tran"
+                    return "5p2"
                 end
 
-                return "3_p1_tran_def"
+                return "5p1"
             end
         },
         {
-            id = "3_p1_tran_def",
-            sound = create_sound("res/music/BGM_Yama_3_P1_Tran_def.ogg"),
+            id = "4p2",
+            sound = create_sound("res/music/BGM_Yama_4P2.ogg"),
+            length = 6631,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "5p3"
+                end
+
+                return "5p2"
+            end
+        },
+        {
+            id = "4p3",
+            sound = create_sound("res/music/BGM_Yama_4P3.ogg"),
+            length = 6631,
+            next_sound_id = "5p3"
+        },
+        {
+            id = "5p1",
+            sound = create_sound("res/music/BGM_Yama_5P1.ogg"),
+            length = 6947,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "6p3"
+                end
+
+                if playerInRange(players[1].uid) then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Player moved into Yama's attack range.")
+                    end
+
+                    return "6p2"
+                end
+
+                return "6p1"
+            end
+        },
+        {
+            id = "5p2",
+            sound = create_sound("res/music/BGM_Yama_5P2.ogg"),
+            length = 6947,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "6p3"
+                end
+
+                return "6p2"
+            end
+        },
+        {
+            id = "5p3",
+            sound = create_sound("res/music/BGM_Yama_5P3.ogg"),
+            length = 6947,
+            next_sound_id = "6p3"
+        },
+        {
+            id = "6p1",
+            sound = create_sound("res/music/BGM_Yama_6P1.ogg"),
+            length = 6631,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "1p3"
+                end
+
+                if playerInRange(players[1].uid) then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Player moved into Yama's attack range.")
+                    end
+
+                    return "1p2"
+                end
+
+                return "1p1"
+            end
+        },
+        {
+            id = "6p2",
+            sound = create_sound("res/music/BGM_Yama_6P2.ogg"),
+            length = 6631,
+            next_sound_id = function(ctx)
+                if isYamaPhaseTwo() then
+                    if module.music_debug_print then
+                        print("[Yama Music Debug] Yama phase2 = true.")
+                    end
+
+                    return "1p3"
+                end
+
+                return "1p2"
+            end
+        },
+        {
+            id = "6p3",
+            sound = create_sound("res/music/BGM_Yama_6P3.ogg"),
+            length = 6631,
+            next_sound_id = "1p3"
+        },
+        {
+            id = "2p1_3p1",
+            sound = create_sound("res/music/BGM_Yama_Tran_2P1_3P1.ogg"),
+            length = 1894,
+            next_sound_id = "3p1"
+        },
+        {
+            id = "2p2_3p2",
+            sound = create_sound("res/music/BGM_Yama_Tran_2P2_3P2.ogg"),
+            length = 1894,
+            next_sound_id = "3p2"
+        },
+        {
+            id = "2px_3p3",
+            sound = create_sound("res/music/BGM_Yama_Tran_2PX_3P3.ogg"),
+            length = 1894,
+            next_sound_id = "3p3"
+        },
+        {
+            id = "3p1_3p2",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P1_3P2.ogg"),
             length = 947,
-            next_sound_id = "4_p1"
+            next_sound_id = "3p2"
         },
         {
-            id = "3_p2",
-            sound = create_sound("res/music/BGM_Yama_3_P2.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "4_p3"
-                end
-
-                return "4_p2"
-            end
+            id = "3p1_3p3",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P1_3P3.ogg"),
+            length = 947,
+            next_sound_id = "3p3"
         },
         {
-            id = "3_p3",
-            sound = create_sound("res/music/BGM_Yama_3_P3.ogg"),
-            length = 6631,
-            next_sound_id = "4_p3"
+            id = "3p1_4p1",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P1_4P1.ogg"),
+            length = 947,
+            next_sound_id = "4p1"
         },
         {
-            id = "4_p1",
-            sound = create_sound("res/music/BGM_Yama_4_P1.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "5_p3"
-                end
-
-                if playerInRange(players[1].uid) then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Player moved into Yama's attack range.")
-                    end
-
-                    return "5_p2"
-                end
-
-                return "5_p1"
-            end
+            id = "3p2_4p2",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P2_4P2.ogg"),
+            length = 947,
+            next_sound_id = "4p2"
         },
         {
-            id = "4_p2",
-            sound = create_sound("res/music/BGM_Yama_4_P2.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "5_p3"
-                end
-
-                return "5_p2"
-            end
+            id = "3p2_4p3",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P2_4P3.ogg"),
+            length = 947,
+            next_sound_id = "4p3"
         },
         {
-            id = "4_p3",
-            sound = create_sound("res/music/BGM_Yama_4_P3.ogg"),
-            length = 6631,
-            next_sound_id = "5_p3"
-        },
-        {
-            id = "5_p1",
-            sound = create_sound("res/music/BGM_Yama_5_P1.ogg"),
-            length = 6947,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "6_p3"
-                end
-
-                if playerInRange(players[1].uid) then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Player moved into Yama's attack range.")
-                    end
-
-                    return "6_p2"
-                end
-
-                return "6_p1"
-            end
-        },
-        {
-            id = "5_p2",
-            sound = create_sound("res/music/BGM_Yama_5_P2.ogg"),
-            length = 6947,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "6_p3"
-                end
-
-                return "6_p2"
-            end
-        },
-        {
-            id = "5_p3",
-            sound = create_sound("res/music/BGM_Yama_5_P3.ogg"),
-            length = 6947,
-            next_sound_id = "6_p3"
-        },
-        {
-            id = "6_p1",
-            sound = create_sound("res/music/BGM_Yama_6_P1.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "1_p3"
-                end
-
-                if playerInRange(players[1].uid) then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Player moved into Yama's attack range.")
-                    end
-
-                    return "1_p2"
-                end
-
-                return "1_p1"
-            end
-        },
-        {
-            id = "6_p2",
-            sound = create_sound("res/music/BGM_Yama_6_P2.ogg"),
-            length = 6631,
-            next_sound_id = function(ctx)
-                if isYamaPhaseTwo() then
-                    if module.music_debug_print then
-                        print("[Yama Music Debug] Yama phase2 = true.")
-                    end
-
-                    return "1_p3"
-                end
-
-                return "1_p2"
-            end
-        },
-        {
-            id = "6_p3",
-            sound = create_sound("res/music/BGM_Yama_6_P3.ogg"),
-            length = 6631,
-            next_sound_id = "1_p3"
+            id = "3p3_4p3",
+            sound = create_sound("res/music/BGM_Yama_Tran_3P3_4P3.ogg"),
+            length = 947,
+            next_sound_id = "4p3"
         }
     }
 }
