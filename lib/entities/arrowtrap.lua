a9e014cdc0e2aeef7b7b93729cc33366b91c2a69
estebanfer <estebanfer2004@gmail.com>
2023-09-13 00:22:07 -0300

    use the custom normal textures ingame

--- lib/entities/arrowtrap.lua
+++ lib/entities/arrowtrap.lua
@@ -1,53 +1,64 @@
 local module = {}
 
 local gold_texture_id
+local gold_normal_texture_id
 local temple_texture_id
 do
-    local temple_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORMISC_0)
-	temple_texture_def.width = 128
-	temple_texture_def.height = 128
-	temple_texture_def.tile_width = 128
-	temple_texture_def.tile_height = 128
-    temple_texture_def.texture_path = "res/arrowtrap_temple.png"
-    temple_texture_id = define_texture(temple_texture_def)
+  local temple_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORMISC_0)
+  temple_texture_def.width = 128
+  temple_texture_def.height = 128
+  temple_texture_def.tile_width = 128
+  temple_texture_def.tile_height = 128
+  temple_texture_def.texture_path = "res/arrowtrap_temple.png"
+  temple_texture_id = define_texture(temple_texture_def)
 
-    local gold_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORMISC_0)
-	gold_texture_def.width = 128
-	gold_texture_def.height = 128
-	gold_texture_def.tile_width = 128
-	gold_texture_def.tile_height = 128
-    gold_texture_def.texture_path = "res/arrowtrap_gold.png"
-    gold_texture_id = define_texture(gold_texture_def)
+  local gold_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORMISC_0)
+  gold_texture_def.width = 128
+  gold_texture_def.height = 128
+  gold_texture_def.tile_width = 128
+  gold_texture_def.tile_height = 128
+  gold_texture_def.texture_path = "res/arrowtrap_gold.png"
+  gold_texture_id = define_texture(gold_texture_def)
+
+  local gold_normal_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORMISC_0)
+  gold_normal_texture_def.width = 128
+  gold_normal_texture_def.height = 128
+  gold_normal_texture_def.tile_width = 128
+  gold_normal_texture_def.tile_height = 128
+  gold_normal_texture_def.texture_path = "res/arrowtrap_gold_normal.png"
+  gold_normal_texture_id = define_texture(gold_normal_texture_def)
 end
 
 function module.create_arrowtrap(x, y, l)
-	removelib.remove_floor_and_embedded_at(x, y, l)
-    local entity = get_entity(spawn_grid_entity(ENT_TYPE.FLOOR_ARROW_TRAP, x, y, l))
-    local left = validlib.is_solid_grid_entity(x-1, y, l)
-    local right = validlib.is_solid_grid_entity(x+1, y, l)
-	local flip = false
-	if not left and not right then
-		if prng:random_chance(2, PRNG_CLASS.LEVEL_GEN) then
-			flip = true
-		end
-	elseif not left then
-		flip = true
-	end
-	if flip == true then
-		flip_entity(entity.uid)
-	end
-	if test_flag(state.level_flags, 18) == true then
-		spawn_entity_over(ENT_TYPE.FX_SMALLFLAME, entity.uid, 0, 0.35)
-	end
+  removelib.remove_floor_and_embedded_at(x, y, l)
+  local entity = get_entity(spawn_grid_entity(ENT_TYPE.FLOOR_ARROW_TRAP, x, y, l))
+  local left = validlib.is_solid_grid_entity(x-1, y, l)
+  local right = validlib.is_solid_grid_entity(x+1, y, l)
+  local flip = false
+  if not left and not right then
+    if prng:random_chance(2, PRNG_CLASS.LEVEL_GEN) then
+      flip = true
+    end
+  elseif not left then
+    flip = true
+  end
+  if flip == true then
+    flip_entity(entity.uid)
+  end
+  if test_flag(state.level_flags, 18) == true then
+    spawn_entity_over(ENT_TYPE.FX_SMALLFLAME, entity.uid, 0, 0.35)
+  end
 
-	if state.theme == THEME.TEMPLE and not options.hd_og_floorstyle_temple then
-		entity:set_texture(temple_texture_id)
-	elseif state.theme == THEME.CITY_OF_GOLD then
-		entity:set_texture(gold_texture_id)
-		entity:set_post_destroy(function (entity)
-			spawn(ENT_TYPE.ITEM_NUGGET, x, y, l, prng:random_float(PRNG_CLASS.AI)*0.2-0.1, prng:random_float(PRNG_CLASS.AI)*0.1+0.1)
-		end)
-	end
+  if state.theme == THEME.TEMPLE and not options.hd_og_floorstyle_temple then
+    entity:set_texture(temple_texture_id)
+  elseif state.theme == THEME.CITY_OF_GOLD then
+    entity:set_texture(gold_texture_id)
+    entity:set_post_destroy(function (entity)
+      spawn(ENT_TYPE.ITEM_NUGGET, x, y, l, prng:random_float(PRNG_CLASS.AI)*0.2-0.1, prng:random_float(PRNG_CLASS.AI)*0.1+0.1)
+    end)
+    entity.rendering_info:set_normal_map_texture(gold_normal_texture_id)
+    entity.rendering_info.shader = 30
+  end
 end
 
 return module
--- lib/entities/tikitrap.lua
+++ lib/entities/tikitrap.lua
@@ -1,6 +1,10 @@
 local module = {}
 local gold_spear_texture_id
 local spear_texture_id
+local gold_trap_texture_id
+local gold_trap_normal_texture_id
+local gold_trap_deco_texture_id
+local gold_trap_deco_normal_texture_id
 do
     local spear_texture_def = TextureDefinition.new()
     spear_texture_def.width = 128
@@ -17,6 +21,31 @@ do
     gold_spear_texture_def.tile_height = 128
     gold_spear_texture_def.texture_path = "res/spear_head_gold.png"
     gold_spear_texture_id = define_texture(gold_spear_texture_def)
+
+    local gold_trap_texture_def = TextureDefinition.new()
+    gold_trap_texture_def.width = 128
+    gold_trap_texture_def.height = 384
+    gold_trap_texture_def.tile_width = 128
+    gold_trap_texture_def.tile_height = 128
+    gold_trap_texture_def.texture_path = "res/tikitrap_gold.png"
+    gold_trap_texture_id = define_texture(gold_trap_texture_def)
+
+    gold_trap_texture_def.texture_path = "res/tikitrap_gold_normal.png"
+    gold_trap_normal_texture_id = define_texture(gold_trap_texture_def)
+
+    gold_trap_texture_def.sub_image_offset_x = 0
+    gold_trap_texture_def.sub_image_offset_y = 256
+    gold_trap_texture_def.sub_image_height = 128
+    gold_trap_texture_def.sub_image_width = 128
+    gold_trap_deco_normal_texture_id = define_texture(gold_trap_texture_def)
+    gold_trap_texture_def.texture_path = "res/tikitrap_gold.png"
+    gold_trap_deco_texture_id = define_texture(gold_trap_texture_def)
+end
+
+local function set_tikitrap_gold_texture(ent)
+    ent:set_texture(gold_trap_texture_id)
+    ent.rendering_info:set_normal_map_texture(gold_trap_normal_texture_id)
+    ent.rendering_info.shader = 30
 end
 
 function module.create_tikitrap(x, y, l)
@@ -27,10 +56,24 @@ function module.create_tikitrap(x, y, l)
     if floor_uid ~= -1 then
         uid = spawn_entity_over(type, floor_uid, 0, 1)
         local s_head = spawn_entity_over(type, uid, 0, 1)
+        if state.theme == THEME.CITY_OF_GOLD then
+            local s_head_ent = get_entity(s_head)
+            set_tikitrap_gold_texture(get_entity(uid))
+            set_tikitrap_gold_texture(s_head_ent)
+            s_head_ent.animation_frame = 0
+        end
     end
 
     return uid
 end
+
+set_post_entity_spawn(function(e)
+    if e.overlay and e.overlay.type.id == ENT_TYPE.FLOOR_LION_TRAP then
+        e:set_texture(gold_trap_deco_texture_id)
+        e.rendering_info:set_normal_map_texture(gold_trap_deco_normal_texture_id)
+        e.rendering_info.shader = 30
+    end
+end, SPAWN_TYPE.ANY, MASK.DECORATION, ENT_TYPE.DECORATION_BRANCH)
 
 set_callback(function()
     if test_flag(state.level_flags, 18) == true then
